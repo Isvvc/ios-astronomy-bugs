6eb1ba0517233ec2a324f56fbf3ad6b77d60db55
Isvvc <lyons@tuta.io>
2019-11-04 11:26:57 -0700

    Fix issue where detail screen has no title
    Closes #3

--- Astronomy/View Controllers/PhotoDetailViewController.swift
+++ Astronomy/View Controllers/PhotoDetailViewController.swift
@@ -36,7 +36,7 @@ class PhotoDetailViewController: UIViewController {
         let dateString = dateFormatter.string(from: photo.earthDate)
         detailLabel.text = "Taken by \(photo.camera.roverId) on \(dateString) (Sol \(photo.sol))"
         cameraLabel.text = photo.camera.fullName
-        title = "\(photo.id)"
+        title = dateString
         
         do {
             let data = try Data(contentsOf: photo.imageURL.usingHTTPS!)
