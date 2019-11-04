eae1bacedfa8c93ee153123b7e5b3a20cccd3164
Isvvc <lyons@tuta.io>
2019-11-04 11:23:55 -0700

    Fix issue where information about a photo would not load if the image fails to load

--- Astronomy/View Controllers/PhotoDetailViewController.swift
+++ Astronomy/View Controllers/PhotoDetailViewController.swift
@@ -32,12 +32,15 @@ class PhotoDetailViewController: UIViewController {
     
     private func updateViews() {
         guard let photo = photo, isViewLoaded else { return }
+        
+        let dateString = dateFormatter.string(from: photo.earthDate)
+        detailLabel.text = "Taken by \(photo.camera.roverId) on \(dateString) (Sol \(photo.sol))"
+        cameraLabel.text = photo.camera.fullName
+        title = "\(photo.id)"
+        
         do {
             let data = try Data(contentsOf: photo.imageURL.usingHTTPS!)
             imageView.image = UIImage(data: data)
-            let dateString = dateFormatter.string(from: photo.earthDate)
-            detailLabel.text = "Taken by \(photo.camera.roverId) on \(dateString) (Sol \(photo.sol))"
-            cameraLabel.text = photo.camera.fullName
         } catch {
             NSLog("Error setting up views on detail view controller: \(error)")
         }
